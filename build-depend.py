dae3bd7921d83cd9d0c6a5fcb9b66aeb149bc48c
ipid <ipid@ipidchan.info>
2018-05-12 16:55:24 +0800

    Add ajax module

--- build-depend.py
+++ build-depend.py
@@ -1,23 +1,24 @@
-# Modules that you may use by require('module').
+# Modules that you may use by require("module").
 # -- MODIFY HERE --
 # modules on npm
 npm_modules = [
     "verify-identity-card",
-    "jquery",
-    "axios",
-    "es6-promise"
+    "jquery"
 ]
 
 # modules written in mods directory.
-# Remember to require with prefix '/mods/'
+# Remember to require with prefix "/mods/"
 local_modules = [    
     "roles",
-    'backend'
+    "backend"
 ]
 
 # Build scripts, DO NOT MODIFY
+uglifyJs = " | uglifyjs -m toplevel -o ./BR-MS/res/dependencies.js"
+
 import os
-requires = ['-r '+x for x in npm_modules] + ['-r ./mods/'+x for x in local_modules]
-os.system(r'browserify ' +
-                ' '.join(requires) +
-                r' -t [babelify --presets env --plugins add-module-exports] | uglifyjs -m toplevel -o ./BR-MS/res/dependencies.js')
+requires = ["-r "+x for x in npm_modules] + ["-r ./mods/"+x for x in local_modules]
+os.system(r"browserify " +
+                " ".join(requires) +
+                r" -t [babelify --presets env --plugins add-module-exports]"
+                + uglifyJs)
